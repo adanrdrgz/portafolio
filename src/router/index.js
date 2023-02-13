cfa21b71b33c13fcfe41b6bec5bb4d71fe15ec01
adanrdrgz <aj.rdrgz444@gmail.com>
2023-02-13 16:59:43 -0300

    fixed

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../pages/Home/Home';
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<App />}>
+            <Route path="/portafolio" element={<App />}>
                 <Route index element={<Home />} /> 
                 <Route path="home" element={<Home />} />
             </Route>
